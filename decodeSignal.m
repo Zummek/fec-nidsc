5b11bdfc5c76102917111cafa74fd6cef12fd6f6
Krzysztof Issel <krzysiekissel@gmail.com>
2020-03-18 21:31:06 +0100

    decodeSignal update

--- decodeSignal.m
+++ decodeSignal.m
@@ -1,23 +1,26 @@
 function [decoded,actualBER] = decodeSignal(signal)
     signalLength = length(signal);
-    decoded = zeros(signalLength/3,1);
+    decoded = zeros(1,signalLength/3);
     j=1; 
     errors =0 ;
     for i=1:3:signalLength
         sum = signal(i)+signal(i+1)+signal(i+2);
         if sum <2
-            if sum ==1
-                errors = errors+1;
-            end
+            
             decoded(j) = 0;
         else
-            if sum ==2
-                errors = errors +1;
-            end
+            
             decoded(j) = 1;
         end
+        
+        if signal(i)==0
+            errors = errors + (signal(i+1)+signal(i+2));
+        else
+            errors = errors + (~signal(i+1)+~signal(i+2));
+        end
+        
         j=j+1;
     end
-    actualBER = errors/length(decoded);
+    actualBER = errors/signalLength;
 end
 
