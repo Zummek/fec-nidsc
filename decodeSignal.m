cad84225f0e211774a35998f89d8666794315ddc
Adam Jędryka <adam.jedryka98@gmail.com>
2020-03-24 19:16:47 +0100

    Fix decodeSignal and add save function to main

--- decodeSignal.m
+++ decodeSignal.m
@@ -1,26 +1,23 @@
 function [decoded,actualBER] = decodeSignal(signal)
     signalLength = length(signal);
     decoded = zeros(1,signalLength/3);
-    j=1; 
-    errors =0 ;
+    j = 1; 
+    errors = 0;
     for i=1:3:signalLength
         sum = signal(i)+signal(i+1)+signal(i+2);
-        if sum <2
-            
+        if sum < 2
             decoded(j) = 0;
         else
-            
             decoded(j) = 1;
         end
         
-        if signal(i)==0
-            errors = errors + (signal(i+1)+signal(i+2));
-        else
-            errors = errors + (~signal(i+1)+~signal(i+2));
+        if !(signal(i) == signal(i+1) && signal(i+1) == signal(i+2)) 
+            errors++;
         end
         
-        j=j+1;
+        j++;
     end
-    actualBER = errors/signalLength;
+    disp(errors);
+    actualBER = errors/signalLength
 end
 
--- main.m
+++ main.m
@@ -1,11 +1,12 @@
 clear all;
 close all;
 
-signalLength = 500;
-BER = 0.1;
+signalLength = 10000;
+BER = 0.01;
 
-signal = generateRandomSignal(signalLength)
-encoded = encodeSignal(signal)
-disturbanced = signalDisturbance(encoded, BER)
-[decoded, actualBER] = decodeSignal(disturbanced)
+signal = generateRandomSignal(signalLength);
+encoded = encodeSignal(signal);
+disturbanced = signalDisturbance(encoded, BER);
+[decoded, actualBER] = decodeSignal(disturbanced);
+save(signalLength, BER, actualBER);
 
