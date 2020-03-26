83a4b53f4eb7fd8e1c51815150d0b0e24a5c70cf
Adam Jędryka <adam.jedryka98@gmail.com>
2020-03-26 17:23:20 +0100

    Add new samples

--- main.m
+++ main.m
@@ -1,12 +1,13 @@
 clear all;
 close all;
 
-signalLength = 10000;
-BER = 0.01;
-
-signal = generateRandomSignal(signalLength);
-encoded = encodeSignal(signal);
-disturbanced = signalDisturbance(encoded, BER);
-[decoded, actualBER] = decodeSignal(disturbanced);
-save(signalLength, BER, actualBER);
-
+signalLength = 5000;
+BER = 0.05;
+for i=0:1:999
+  disp(i)
+  signal = generateRandomSignal(signalLength);
+  encoded = encodeSignal(signal);
+  disturbanced = signalDisturbance(encoded, BER);
+  [decoded, actualBER] = decodeSignal(disturbanced);
+  save(signalLength, BER, actualBER);
+end
