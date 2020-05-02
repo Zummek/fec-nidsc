1aa3c501c8bb7d2f4d1655058b0278a697ec7020
Aleksandra Zdrojowa <a.zdrojowa@gmail.com>
2020-05-03 01:26:11 +0200

    add more data

--- main.m
+++ main.m
@@ -7,7 +7,7 @@ BCHBase = 8;
 packageLength = 2^BCHBase - 1;
 dataBitLength = 247;
 BCHCorrection = 1;
-actualBER = 0.2;
+actualBER = 0.3;
 
 % Main loop for tests
 for i=0:2000
@@ -20,7 +20,7 @@ for i=0:2000
   [nr, nc] = size (decoded);
   bitsAmount = (nr*nc);
   E = (bitsAmount - errAmount) / bitsAmount;
-  writeToFile([E, errAmount, BER], "data_8_247_1_0.2");    % data_BCHBAse_DataBitLength_BCHcorrection_actualBer (actual BER to prawdopodobieñstwo przek³amania tych bitów na poczatku) 
+  writeToFile([E, errAmount, BER], "data_8_247_1_0.3");    % data_BCHBAse_DataBitLength_BCHcorrection_actualBer (actual BER to prawdopodobieñstwo przek³amania tych bitów na poczatku) 
   % debugger :) 
   %disp("data: "), disp(data);
   %disp("disturbanced: "), disp(disturbanced);
